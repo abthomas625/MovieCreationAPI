c584bc312e4d5f2330904269ad4d6ab9bce31a30
Abilena <abthomas625@gmail.com>
2022-11-02 21:22:40 -0500

    It's movie night

---
+++ MovieCreationAPI/Controllers/MoviesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieCreationAPI.Models;
+
+namespace MovieCreationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MoviesContext _context;
+
+        public MoviesController(MoviesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            return await _context.Movies.ToListAsync();
+        }
+
+        // GET: api/Movies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+        // PUT: api/Movies/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Movies
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/Movies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+        //Get a random movie pick
+        [HttpGet("GetRandomMovie")]
+        public async Task<ActionResult<Movie>> GetRandomMovie()
+        {
+            Random random = new Random();
+            List<Movie> movies = _context.Movies.ToList();
+            int id = random.Next(movies.Count);
+
+            Movie m = movies[id];
+            return m;
+        }
+        //Get a random movie pick from a specific category
+        //User specifies category as a query parameter
+        [HttpGet("GetRandomMovieByGenre/{genre}")]
+        public async Task<ActionResult<Movie>> GetRandomMovieByGenre(string genre)
+        {
+            Random random = new Random();
+            List<Movie> movieByGenre = new List<Movie>();
+            List<Movie> movies = _context.Movies.ToList();
+            foreach(Movie m in movies)
+            {
+                if(m.Genre == genre)
+                {
+                    movieByGenre.Add(m);
+                }
+            }
+            int id = random.Next(movieByGenre.Count);
+            Movie movie  = movieByGenre[id];
+            return movie;
+        }
+    }
+}
